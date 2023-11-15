f9a57df525d522c5bd070a8dd4a06f9da45709e3
Romero Yaguana Anthony Joel <romeroyaguanaanthonyjoel@gmail.com>
2023-11-15 15:11:29 -0500

    Cambio numero 2 toLowerCase y toUpperCase

--- ejemplos-switch/EjemplosSwitch/src/manejocadenas/Ejemplo02.java
+++ ejemplos-switch/EjemplosSwitch/src/manejocadenas/Ejemplo02.java
@@ -14,8 +14,9 @@ public class Ejemplo02 {
     public static void main(String[] args) {
         String pais;
         
-        pais = "ocuador";
-        char letra = pais.charAt(0);
+        pais = "Ecuador";
+        //      0123456
+        char letra = pais.charAt(0);//CharAt me permite presentar un caracter especifico de una variable string 
         System.out.printf("%s\n", letra);
         
     }
--- ejemplos-switch/EjemplosSwitch/src/manejocadenas/EjemploCadenas.java
+++ ejemplos-switch/EjemplosSwitch/src/manejocadenas/EjemploCadenas.java
@@ -14,8 +14,10 @@ public class EjemploCadenas {
         String ciudad = "loJa";
         ciudad = ciudad.toUpperCase();
         System.out.printf("%s\n", ciudad.toLowerCase());
-        System.out.printf("%s\n", ciudad.toUpperCase());
-        System.out.printf("%s\n", ciudad);
+        //System.out.printf("%s\n", ciudad.toUpperCase());
+        System.out.printf("%s\n", ciudad); 
+        // toLowerCase es un metodo que me permite sobrescribir un caracter a letra minusculas
+        // toUpperCase es un metodo que me permite sobrescribir un caracter a letra mayusculas 
         
         
     }
