ade3935678b1ed7c811202516d8d34bc4701a5a3
Romero Yaguana Anthony Joel <romeroyaguanaanthonyjoel@gmail.com>
2023-11-15 14:59:37 -0500

    Cambio numero 1 CharAt

--- ejemplos-switch/EjemplosSwitch/src/manejocadenas/Ejemplo02.java
+++ ejemplos-switch/EjemplosSwitch/src/manejocadenas/Ejemplo02.java
@@ -14,7 +14,7 @@ public class Ejemplo02 {
     public static void main(String[] args) {
         String pais;
         
-        pais = "Ecuador";
+        pais = "ocuador";
         char letra = pais.charAt(0);
         System.out.printf("%s\n", letra);
         
